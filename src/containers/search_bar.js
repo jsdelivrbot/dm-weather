9c5193cf1b204f1d086dbe8f5af77e3bcbc93013
Dave Moran <dave.moran118@gmail.com>
2017-01-11 12:24:55 -0500

    Change bind declaration

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -7,9 +7,11 @@ export default class SearchBar extends Component {
     this.state = {
       term: ''
     }
+
+    this.onInputChange = this.onInputChange.bind(this)
   }
 
-  onInputChange = (event) => {
+  onInputChange(event) {
     console.log(event.target.value);
     this.setState({
       term: event.target.value
